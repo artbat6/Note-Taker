a2398ef93b95b83e3b7e702832ecdb4bea0aa0f7
artbat6 <arthurbatesdev@gmail.com>
2021-04-26 14:57:52 -0500

    added routes

--- server.js
+++ server.js
@@ -13,3 +13,23 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+// Routes
+// =============================================================
+
+// sends the user to the index page
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+  });
+  
+  // sends the user to the notes page
+  app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+  });
+  
+  // sends the user the db.json file
+  app.get("/api/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "db.json"));
+  });
+  
+  
