119985d75c44483fb884b16f44189caf2ec43878
artbat6 <arthurbatesdev@gmail.com>
2021-04-27 18:29:16 -0500

    declared some note variables

---
+++ public/assets/js/index.js
@@ -0,0 +1,16 @@
+var $noteTitle = $(".note-title");
+var $noteText = $(".note-textarea");
+var $saveNoteBtn = $(".save-note");
+var $newNoteBtn = $(".new-note");
+var $noteList = $(".list-container .list-group");
+
+// activeNote is used to keep track of the note in the textarea
+var activeNote = {};
+
+// A function for getting all notes from the db
+var getNotes = function() {
+  return $.ajax({
+    url: "/api/notes",
+    method: "GET"
+  });
+};
